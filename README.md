e1cbd97fbe65dd9b125e126d877f241d009782d8
Philip Quach <philipquach@outlook.com>
2020-01-04 13:48:29 -0600

    cleaned up code and added more description

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Python Guessing Game
 
 ## Introduction
 This is a game that uses the random module.
+The user has 6 tries to guess the correct number.
 The AI random picks a number and the user will be able to guess. 
 The AI will inform the user if the number guessed is too high or too low.
--- guess.py
+++ guess.py
@@ -1,18 +1,19 @@
-import random
+from random import randint
 
 # Generate random number from 1 to 100
-x = random.randint(1, 101)
+x = randint(1, 101)
 count = 6
 tries = 0
 
-while True:
+while count > 0:
 
     print('Press q to quit')
-    guess = input('Guess a number from 1 to 100 with 6 tries: ')
+    guess = input('Guess a number from 1 to 100: ')
 
     if guess == 'q':  # Quit the game if user types q
         print('Number was {}'.format(x))
         break
+
     if int(guess) == x:
         print('You won! Took you  {} of tries'.format(tries))
         break
@@ -24,8 +25,8 @@ while True:
         count -= 1
         tries += 1
         print('Too small - {} of tries left'.format(count))
+
     if count == 0:
         print('You lose')
-        break
 
 
