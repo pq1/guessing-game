ded1b08b77e3a40b6adc0c1c12adab6c4ab9b4ca
Philip Quach <philipquach@outlook.com>
2020-01-03 21:08:07 -0600

    added in more detail to intial

--- guess.py
+++ guess.py
@@ -5,8 +5,9 @@ x = random.randint(1, 101)
 
 while True:
     print('Press q to quit')
-    guess = input('Guess a number: ')
+    guess = input('Guess a number from 1 to 100: ')
     if guess == 'q':
+        print('Number was {}'.format(x))
         break
     elif int(guess) == x:
         print('You won!')
