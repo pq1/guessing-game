d44c4c23615a7fe594a95b240f78317515446b2d
Philip Quach <philipquach@outlook.com>
2020-01-03 23:08:28 -0600

    allow users limited tries

--- guess.py
+++ guess.py
@@ -2,19 +2,26 @@ import random
 
 # Generate random number from 1 to 100
 x = random.randint(1, 101)
+count = 6
 
 while True:
     print('Press q to quit')
-    guess = input('Guess a number from 1 to 100: ')
-    if guess == 'q':
+    guess = input('Guess a number from 1 to 100 with 6 tries: ')
+
+    if guess == 'q':  # Quit the game if user types q
         print('Number was {}'.format(x))
         break
     if int(guess) == x:
-        print('You won!')
+        print('You won! Took you  {} of tries'.format(count))
         break
     elif int(guess) > x:
-        print('Too big')
+        count -= 1
+        print('Too big - {} of tries left'.format(count))
     else:
-        print('Too small')
+        count -= 1
+        print('Too small - {} of tries left'.format(count))
+    if count == 0:
+        print('You lose')
+        break
 
 
