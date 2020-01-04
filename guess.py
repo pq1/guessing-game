ed53c37c111f0d23a8c273427996d39313cd5881
Philip Quach <philipquach@outlook.com>
2020-01-03 23:17:39 -0600

    more intelligent to let user know if number is too big or too small.

--- guess.py
+++ guess.py
@@ -3,8 +3,10 @@ import random
 # Generate random number from 1 to 100
 x = random.randint(1, 101)
 count = 6
+tries = 0
 
 while True:
+
     print('Press q to quit')
     guess = input('Guess a number from 1 to 100 with 6 tries: ')
 
@@ -12,13 +14,15 @@ while True:
         print('Number was {}'.format(x))
         break
     if int(guess) == x:
-        print('You won! Took you  {} of tries'.format(count))
+        print('You won! Took you  {} of tries'.format(tries))
         break
     elif int(guess) > x:
         count -= 1
+        tries += 1
         print('Too big - {} of tries left'.format(count))
     else:
         count -= 1
+        tries += 1
         print('Too small - {} of tries left'.format(count))
     if count == 0:
         print('You lose')
